1db1d6b1628232076652204c0d684f61ec14a8a1
Cleyton Bonamigo <cleytonbonamigo@gmail.com>
2023-07-03 19:38:00 -0300

    Send base implements to an AbstractController.

--- src/Tweet.php
+++ src/Tweet.php
@@ -4,24 +4,6 @@ namespace CleytonBonamigo\ShareTwitter;
 
 class Tweet
 {
-    /** @var int */
-    private int $account_id;
-
-    /** @var string */
-    private string $access_token;
-
-    /** @var string */
-    private string $access_token_secret;
-
-    /** @var string */
-    private string $consumer_key;
-
-    /** @var string */
-    private string $consumer_secret;
-
-    /** @var string */
-    private string $bearer_token;
-
     /**
      * Construct the Tweet class.
      * @param array<string> $settings
@@ -29,31 +11,17 @@ class Tweet
      */
     public function __construct(array $settings)
     {
-        $this->parseSettings($settings);
+        parent::__construct($settings);
     }
 
     /**
-     * @param array<string> $settings
-     * @return void
-     * @throws \Exception
+     * Create a Tweet
+     * See https://developer.twitter.com/en/docs/twitter-api/tweets/manage-tweets/api-reference/post-tweets
+     * @return $this
      */
-    public function parseSettings(array $settings): void
+    public function create(): Tweet
     {
-        if(!isset($settings['account_id'],
-            $settings[ 'access_token'],
-            $settings['access_token_secret'],
-            $settings['consumer_key'],
-            $settings['consumer_secret'],
-            $settings['bearer_token']
-        )){
-            throw new \Exception('Incomplete settings');
-        }
-
-        $this->account_id = (int)$settings['account_id'];
-        $this->access_token = $settings['access_token'];
-        $this->access_token_secret = $settings['access_token_secret'];
-        $this->consumer_key = $settings['consumer_key'];
-        $this->consumer_secret = $settings['consumer_secret'];
-        $this->bearer_token = $settings['bearer_token'];
+        $this->setEndpoint('tweets');
+        return $this;
     }
 }
