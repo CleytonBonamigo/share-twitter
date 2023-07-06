126f35813d60fb1c0e5d4e28728efc303ae0f6f7
Cleyton Bonamigo <cleytonbonamigo@gmail.com>
2023-07-06 19:22:05 -0300

    Perform tests.

---
+++ tests/ImageTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CleytonBonamigo\ShareTwitter\Tests;
+
+class ImageTest extends BaseTestCase
+{
+    /**
+     * Upload an Image
+     * @return void
+     */
+    public function testUploadImage(): void
+    {
+        $this->uploadImage();
+    }
+}
---
+++ tests/TweetTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace CleytonBonamigo\ShareTwitter\Tests;
+
+use Exception;
+
+class TweetTest extends BaseTestCase
+{
+    /**
+     * Post new Tweet.
+     * @return void
+     */
+    public function testPostTweet(): void
+    {
+        try{
+            $date = new \DateTime('NOW');
+            $response = $this->client->tweet()->create([
+                'text' => "Test Tweet {$date->format(\DateTimeInterface::ATOM)}"
+            ]);
+
+            self::assertTrue(is_object($response) && property_exists($response, 'data'));
+        }catch (Exception $e){
+            $this->fail("Test failed: {$e->getMessage()}\n{$e->getTraceAsString()}");
+        }
+    }
+
+    /**
+     * Post a Tweet with Image
+     * @return void
+     */
+    public function testPostTweetWithImage(): void
+    {
+        try{
+            $newImage = $this->uploadImage();
+
+            $date = new \DateTime('NOW');
+            $response = $this->client->tweet()->create([
+                'text' => "Test Tweet with image {$date->format(\DateTimeInterface::ATOM)}",
+                'media' => [
+                    'media_ids' => [$newImage->media_id_string]
+                ]
+            ]);
+
+            self::assertTrue(is_object($response) && property_exists($response, 'data'));
+        }catch (Exception $e){
+            $this->fail("Test failed: {$e->getMessage()}\n{$e->getTraceAsString()}");
+        }
+    }
+}
