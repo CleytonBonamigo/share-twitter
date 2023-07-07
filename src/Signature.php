8f03db283a35d705b91aa1c68940f0b066341ef9
Cleyton Bonamigo <cleytonbonamigo@gmail.com>
2023-07-06 22:57:09 -0300

    Fix the Signature.

--- src/Signature.php
+++ src/Signature.php
@@ -3,7 +3,6 @@
 namespace CleytonBonamigo\ShareTwitter;
 
 use CleytonBonamigo\ShareTwitter\Enums\Action;
-use CleytonBonamigo\ShareTwitter\Enums\Methods;
 use CleytonBonamigo\ShareTwitter\Utils\Util;
 
 class Signature
@@ -14,17 +13,14 @@ class Signature
     /** @var string */
     protected string $url;
 
-    /** @var Action */
-    protected Action $action;
-
-    /** @var Methods */
-    protected Methods $method;
+    /** @var AbstractController */
+    protected AbstractController $controller;
 
     /**
      * Constructor of the class.
-     * @param array $config
+     * @param AbstractController $controller
      */
-    public function __construct(array $config = [])
+    public function __construct(AbstractController $controller)
     {
         $this->config = [
             'version'          => '1.0',
@@ -33,45 +29,21 @@ class Signature
             'signature_method' => 'HMAC-SHA1',
         ];
 
-        foreach ($config as $key => $value) {
+        foreach ($controller->getConfigs() ?? [] as $key => $value) {
             $this->config[$key] = $value;
         }
+
+        $this->controller = $controller;
+        $this->setUrl();
     }
 
     /**
-     * Se URL.
-     * @param string $url
-     * @return $this
+     * Set the URL.
+     * @return void
      */
-    public function setUrl(string $url): Signature
+    public function setUrl(): void
     {
-        $this->url = str_replace(['https://', 'http://'], '', $url);
-
-        return $this;
-    }
-
-    /**
-     * Set the action.
-     * @param Action $action
-     * @return $this
-     */
-    public function setAction(Action $action): Signature
-    {
-        $this->action = $action;
-
-        return $this;
-    }
-
-    /**
-     * Set the Method
-     * @param Methods $method
-     * @return $this
-     */
-    public function setMethod(Methods $method): Signature
-    {
-        $this->method = $method;
-
-        return $this;
+        $this->url = $this->controller->getApiBaseUrl().$this->controller->getEndpoint();
     }
 
     /**
@@ -79,11 +51,10 @@ class Signature
      * @param array $postfields
      * @return string
      */
-    public function getAuthorizationSignature(array $postfields = [])
+    public function authorize(array $postfields = []): string
     {
         $nonce = $this->generateNonce();
         $timestamp = time();
-        $signatureMethod = 'HMAC-SHA1';
 
         $signableParams = [
             'oauth_version' => $this->config['version'],
@@ -94,14 +65,14 @@ class Signature
             'oauth_signature_method' => $this->config['signature_method']
         ];
 
-        if(!($this->action === Action::POST_TWITTER)){
+        if(!($this->controller->getAction() === Action::POST_TWITTER)){
             $signableParams = Util::buildHttpQuery(array_merge($signableParams, $postfields));
         }else{
             $signableParams = Util::buildHttpQuery($signableParams);
         }
 
         $parts = [
-            strtoupper($this->method->value),
+            strtoupper($this->controller->getMethod()->value),
             $this->url,
             $signableParams
         ];
@@ -114,9 +85,8 @@ class Signature
         $signature = Util::urlencodeRfc3986(base64_encode(hash_hmac('sha1', $signatureBase, $key, true)));
 
         return 'Authorization: OAuth oauth_consumer_key="'.$this->config['consumer_key'].'", oauth_nonce="'.$nonce
-            .'", oauth_signature="'.$signature.'", oauth_signature_method="'.$this->config['signature_method']
-            .'", oauth_timestamp="'.$timestamp.'", oauth_token="'.$this->config['access_token']
-            .'", oauth_version="'.$this->config['version'].'"';
+            .'", oauth_signature="'.$signature.'", oauth_signature_method="'.$this->config['signature_method'].'", oauth_timestamp="'.$timestamp.'"'.
+            ', oauth_token="'.$this->config['access_token'].'", oauth_version="'.$this->config['version'].'"';
     }
 
     /**
@@ -125,6 +95,15 @@ class Signature
      */
     protected function generateNonce(): string
     {
-        return sha1(uniqid('', true) . $this->url);
+        return sha1(uniqid('', true) . $this->getSanitizedUrl());
+    }
+
+    /**
+     * Remove https:// and http:// for nonce.
+     * @return string
+     */
+    protected function getSanitizedUrl(): string
+    {
+        return str_replace(['https://', 'http://'], '', $this->url);
     }
 }
